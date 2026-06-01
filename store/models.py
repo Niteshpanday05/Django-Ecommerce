965d710afa579122abedce3699ef464834124292
Niteshpanday05 <eagleseye.np@gmail.com>
2026-06-01 20:36:57 +0545

    problem of stiore models solved

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from category.models import Category
 # Create your models here.
 
 class Product(models.Model):
@@ -7,8 +8,8 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
-    image = models.IntegerField(upload_to='photos/products')
+    image = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default = True)
-    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
