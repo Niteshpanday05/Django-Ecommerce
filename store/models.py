d8342dc1f4a9d4fd223af632d11d1592ec641752
Niteshpanday05 <eagleseye.np@gmail.com>
2026-06-01 20:50:11 +0545

    product features is added

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
 from category.models import Category
+
 # Create your models here.
 
 class Product(models.Model):
@@ -12,4 +12,9 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default = True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
     
+    
+    def __str__(self):
+        return self.product_name
