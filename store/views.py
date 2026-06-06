6af3fed89206fc0557fa4277ed6ebb6fcd25477d
Niteshpanday05 <eagleseye.np@gmail.com>
2026-06-06 11:06:27 +0545

    category by productrporblem solved

--- store/views.py
+++ store/views.py
@@ -7,19 +7,17 @@ def store(request, category_slug=None):
     categories=None
     products=None
     
-    if category_slug!=None:
+    if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products= Product.objects.filter(category=categories, is_available=True)
+        product_count=products.count()
+         
     else:
         products=Product.objects.all().filter(is_available=True)
         product_count=products.count()
-    
-    
-    products = Product.objects.all().filter(is_available = True)
-    product_count = products.count()
     
     context = {
         'products' : products,
         'product_count': product_count,
     }
-    return render (request,'store/store.html', context)
+    return render (request, 'store/store.html', context)
